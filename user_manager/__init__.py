1cf1d5dae4c1391eb079f94cec1aff740e4be32a
Jillian Vogel <jill@opencraft.com>
2018-08-27 02:07:58 +0930

    Bump version for new tag (#5)

--- user_manager/__init__.py
+++ user_manager/__init__.py
@@ -4,6 +4,6 @@ an app provides an API to manage user-manager relationships in Open edX.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 default_app_config = 'user_manager.apps.UserManagerAppConfig'  # pylint: disable=invalid-name
